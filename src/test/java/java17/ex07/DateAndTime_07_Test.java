54e8bb687bbe93cb382c9b3e35a6867e177b40e5
Elshindr <headkeytics@gmail.com>
2023-06-16 17:26:31 +0200

    TP 6

--- src/test/java/java17/ex07/DateAndTime_07_Test.java
+++ src/test/java/java17/ex07/DateAndTime_07_Test.java
@@ -3,8 +3,7 @@ package java17.ex07;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.time.LocalDate;
-import java.time.Month;
+import java.time.*;
 import java.util.Date;
 
 import org.junit.Test;
@@ -18,13 +17,13 @@ public class DateAndTime_07_Test {
 	public void test_date_to_localdate() throws Exception {
 
 		// TODO Créer une date Java 1 (12/02/2017)
-		Date date = null;
+		Date date = new Date(117, 1, 12);
 
 		// TODO transformer la date en Instant
-
+		Instant ins = date.toInstant();
 		// TODO transformer la date en LocalDate
 
-		LocalDate result = null;
+		LocalDate result = LocalDate.ofInstant(ins, ZoneId.systemDefault());
 
 		// TODO valoriser les différentes variables afin de rendre le test passant
 
